aa4eded1f8a16322456341da707e4dbe4c8fdff1
Benjamin Løndal <benjamin.londal@gmail.com>
2022-06-02 23:51:02 +0200

    update on product specific
    made queryString work!

--- js/product-specific.js
+++ js/product-specific.js
@@ -3,17 +3,15 @@ import { games } from "./data/games.js";
 const detailContainer = document.querySelector(".detail-container");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const id = params.get("id");
-
-const game = games.find( ({ id }) => id == params);
+const game = games.find( ({ id }) => id == params.get("id"));
 
 console.log(game);
 
-document.title = `${games.name} | GameHub`;
+document.title = `${game.title} | GameHub`;
 
-detailContainer.innerHTML += `<div class="product_content">
-                                    <img src="${games.image}">
-                                    <h2>${games.title}(${games.year})</h2>
-                                    <p>$${games.price}</p>
-                                    <a href="product_specific.html" class="cta"> VIEW GAME </a>
+detailContainer.innerHTML += `<div class="product_specific">
+                                <img src="${game.image}" alt="Racing game" class="product_content_specific">
+                                <h1>${game.title} (${game.year})</h1>
+                                <p class="product_specific_info">${game.about}</p>
+                                <p class="cost">Price: ${game.price}</p>
                             </div>`;
