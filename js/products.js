bc3f8947ec96ca47de8a1a38d31de44fb7e53a27
Benjamin Løndal <benjamin.londal@gmail.com>
2022-06-03 15:08:06 +0200

    update

--- js/products.js
+++ js/products.js
@@ -6,7 +6,8 @@ let gamesToRender = games;
 function renderGames() {
     gamesToRender.forEach(function(games) {
         gameContainer.innerHTML += `<div class="product_content">
-                                        <img src="${games.image}">
+                                        <a href="product_specific.html?id=${games.id}">
+                                        <img src="${games.image}" alt="cover of ${games.title}"></a>
                                         <h2>${games.title}(${games.year})</h2>
                                         <p>$${games.price}</p>
                                         <a href="product_specific.html?id=${games.id}" class="cta"> VIEW GAME </a>
