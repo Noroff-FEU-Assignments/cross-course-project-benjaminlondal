0183ddd4d0f79315bd9cdfa10e05f8c3f4745506
Benjamin Løndal <benjamin.londal@gmail.com>
2022-06-03 11:29:32 +0200

    updates

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,18 @@
 const shopOverlay = document.getElementById("shop-overlay");
+const cartDropDown = document.querySelector(".cart");
+const cartIcon = document.getElementById("grid-cart");
 
 function displayShopOverlay() {
     shopOverlay.style.display = "block";
 }
+
+function displayCartDropDown() {
+    if (cartDropDown.style.display === "none") {
+        cartDropDown.style.display = "block";
+      } else {
+        cartDropDown.style.display = "none";
+      }
+    
+}
+
+cartIcon.onclick = displayCartDropDown;
--- js/product-specific.js
+++ js/product-specific.js
@@ -5,8 +5,6 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const game = games.find( ({ id }) => id == params.get("id"));
 
-console.log(game);
-
 document.title = `${game.title} | GameHub`;
 
 detailContainer.innerHTML += `<div class="product_specific">
@@ -14,4 +12,20 @@ detailContainer.innerHTML += `<div class="product_specific">
                                 <h1>${game.title} (${game.year})</h1>
                                 <p class="product_specific_info">${game.about}</p>
                                 <p class="cost">Price: $${game.price}</p>
+                                <button class="cta" data-game=${game.id}> Add to Cart </button>
                             </div>`;
+
+
+
+const button = document.querySelector("button");
+//var cartArray = [];
+
+//function addArray(event) {
+    
+//    const itemToAdd = games.find(item => item.id === event.target.dataset.game);
+//    cartArray.push(itemToAdd);
+//}
+
+button.onclick = displayShopOverlay, displayCartDropDown;
+
+//console.log(cartArray)
